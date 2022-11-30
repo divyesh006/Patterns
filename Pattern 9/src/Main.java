f046a91a01fa579b6bec7b9c6ac14692f2230cf6
divyesh006 <divyesh10008@gmail.com>
2022-11-30 18:22:01 +0530

    Pattern9

---
+++ Pattern 9/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    static void pattern(int n){
+        for (int row = 1; row <= n; row++){
+            int spaceInRow =  row - 1;
+            for (int space = 1; space <= spaceInRow; space++){
+                System.out.printf("  ");
+            }
+            int colInRow = 2 * n - row - spaceInRow;
+            for (int col = 1; col <= colInRow; col++){
+                System.out.printf("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(5);
+    }
+}
+
+/*
+             * * * * * * * * *
+               * * * * * * *
+                 * * * * *
+                   * * *
+                     *
+ */
